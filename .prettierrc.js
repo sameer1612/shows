8216fa3f9f7cbf5758cd83e15f37a77d8c37e0d3
Sameer Kumar <kumar.sam1612@gmail.com>
2025-02-20 12:05:22 +0530

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  usetabs: false,
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+};
